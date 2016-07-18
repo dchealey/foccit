c13e5fe093195afef26356bb52c753e9baa96ce0
David <davidchristopherhealey@gmail.com>
2016-07-18 03:29:31 +0000

    Assignment 33 Complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
 
+  get 'question/index'
+
+  get 'question/show'
+
+  get 'question/new'
+
+  get 'question/edit'
+
+  get 'question/create'
+
+  get 'question/update'
+
+  get 'question/destroy'
+
+  resources :questions
+
   resources :posts
 
   get 'about' => 'welcome#about'
