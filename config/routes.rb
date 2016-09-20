548c5798c2ac36d6a6b58146449b9f9a6f127179
David <davidchristopherhealey@gmail.com>
2016-09-20 02:47:26 +0000

    Checkpoint 45: completed

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
 
   resources :sessions, only: [:new, :create, :destroy]
 end
